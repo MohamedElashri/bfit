e4b7b44442df3328ec599025bf670ee0fd61d0da
Mohamed Elashri <mail@elashri.com>
2024-07-11 02:31:32 -0400

    Add unbinned fit
    1. A new UnbinnedFitBase class:
    
    - The class is parallel to `FitBase` but designed for unbinned data.
    Implement a `log_likelihood` method instead of chi_squared.
    Modify the `_setup_fit` method to handle unbinned data.
    
    2. Modify the `Component` class:
    - Add a `pdf` method to each component that returns the probability density function.
    - This allows to reuse components for both binned and unbinned fits.
    
    3. Add an `UnbinnedCompositeModel` class:
    - Similar to `CompositeModel`, but uses the log-likelihood for fitting.
    
    4. Implement unbinned versions of existing fit models:
    - These inherits from `UnbinnedCompositeModel` and reuse the existing components.
    
    5. For `fit` method inside `UnbinnedCompositeModel`, Accept an array of individual data points and use maximum likelihood estimation instead of chi-squared minimization.

--- bfit.py
+++ bfit.py
@@ -2,34 +2,31 @@ import numpy as np
 import matplotlib.pyplot as plt
 from iminuit import Minuit
 from scipy.integrate import quad
+from scipy.stats import rv_continuous
 
 class FitModels:
     """Contains various probability distribution functions and background models."""
     
-    def __init__(self, bin_width, x_min, x_max):
+    def __init__(self, bin_width=1, x_min=0, x_max=1):
         self.bin_width = bin_width
         self.x_min = x_min
         self.x_max = x_max
 
     def gaussian(self, x, mean, std_dev):
         """Gaussian distribution."""
-        return self.bin_width * np.exp(-(x - mean) ** 2 / (2 * std_dev ** 2)) / np.sqrt(2 * np.pi * std_dev ** 2)
+        return np.exp(-(x - mean) ** 2 / (2 * std_dev ** 2)) / np.sqrt(2 * np.pi * std_dev ** 2)
 
     def exponential(self, x, amplitude, decay):
         """Exponential distribution."""
-        integral = (amplitude / decay) * (1.0 - np.exp(-decay * (self.x_max - self.x_min)))
-        norm = 1. / integral
-        return self.bin_width * norm * amplitude * np.exp(-decay * (x - self.x_min))
+        return amplitude * np.exp(-decay * (x - self.x_min))
 
     def linear(self, x, slope, intercept):
         """Linear function."""
-        integral = slope * (self.x_max - self.x_min) + 0.5 * intercept * (self.x_max ** 2 - self.x_min ** 2)
-        norm = 1. / integral
-        return self.bin_width * norm * (slope * x + intercept)
+        return slope * x + intercept
 
     def parabola(self, x, a, b, c):
         """Parabolic function."""
-        return self.bin_width * (a * (x ** 2) + b * x + c)
+        return a * (x ** 2) + b * x + c
 
     def breit_wigner(self, x, mass, width):
         """Breit-Wigner distribution."""
@@ -45,28 +42,25 @@ class FitModels:
 
     def argus_bg(self, x, m0, c, p):
         """ARGUS background function."""
-        def argus_bg_integral(m0, c, p):
-            def integrand(x):
-                z = 1 - (x / m0) ** 2
-                return x * np.sqrt(z) * np.exp(c * z ** p) if z > 0 else 0
-            integral, _ = quad(integrand, 0, m0, limit=10000)
-            return integral
-        
-        normalization = argus_bg_integral(m0, c, p)
         z = 1 - (x / m0) ** 2
-        return np.where(z > 0, (x * np.sqrt(z) * np.exp(c * z ** p)) / normalization, 0)
+        return np.where(z > 0, x * np.sqrt(z) * np.exp(c * z ** p), 0)
 
-class Component:
+class Component(rv_continuous):
     """Base class for fit components."""
-    def __init__(self, function, n_params):
+    def __init__(self, function, pdf, n_params):
+        super().__init__()
         self.function = function
+        self._pdf = pdf
         self.n_params = n_params
 
     def __call__(self, x, *params):
         return self.function(x, *params)
 
+    def pdf(self, x, *params):
+        return self._pdf(x, *params)
+
 class FitBase:
-    """Base class for all fit models."""
+    """Base class for all binned fit models."""
 
     def __init__(self, bins, counts, param_limits=None):
         self.bins = bins
@@ -191,36 +185,155 @@ class CompositeModel(FitBase):
             param_index += n_params
         return result
 
+class UnbinnedFitBase:
+    """Base class for all unbinned fit models."""
+
+    def __init__(self, data, param_limits=None):
+        self.data = data
+        self.x_min, self.x_max = self._setup_fit(data)
+        self.param_limits = param_limits or {}
+        self.fit_models = FitModels(x_min=self.x_min, x_max=self.x_max)
+        self.fit_result = None
+
+    def _setup_fit(self, data):
+        return np.min(data), np.max(data)
+
+    def log_likelihood(self, *params):
+        """Calculate log-likelihood value."""
+        raise NotImplementedError("Subclass must implement abstract method")
+
+    def fit(self, initial_params, param_names=None):
+        """Perform the fit."""
+        if param_names is None:
+            param_names = [f'param_{i}' for i in range(len(initial_params))]
+        assert len(param_names) == len(initial_params), "Number of parameter names must match the number of initial parameters."
+        
+        minuit = Minuit(self.log_likelihood, name=param_names, *initial_params)
+        for key, value in self.param_limits.items():
+            minuit.limits[key] = value
+        minuit.migrad()
+        self.fit_result = minuit
+        return minuit
+
+    def plot(self, title='Fit plot', xlabel='X', ylabel='Y', vlines=None, plot_range=None,
+             data_color='b', data_label='Data', fit_color='r', fit_label='Fit', show_plot=True):
+        """Plot the fit results."""
+        fig, ax = plt.subplots()
+        ax.set_title(title)
+
+        if plot_range is not None:
+            ax.set_xlim(plot_range)
+        else:
+            ax.set_xlim(self.x_min, self.x_max)
+
+        ax.hist(self.data, bins=100, density=True, color=data_color, label=data_label)
+
+        if self.fit_result:
+            dense_x_vals = np.linspace(self.x_min, self.x_max, 1000)
+            predictions = self.pdf(dense_x_vals, *self.fit_result.values)
+            ax.plot(dense_x_vals, predictions, color=fit_color, label=fit_label)
+
+        ax.set_xlabel(xlabel)
+        ax.set_ylabel(ylabel)
+        ax.grid(True)
+        
+        if vlines:
+            for vl in vlines:
+                ax.vlines(vl, 0., 0.6 * ax.get_ylim()[1], colors='yellow')
+        
+        ax.legend()
+
+        if show_plot:
+            plt.show()
+
+        return fig, ax
+
+    def summary(self):
+        """Print fit summary."""
+        if self.fit_result is not None:
+            print(self.fit_result)
+        else:
+            print("Fit has not been performed yet.")
+
+    @property
+    def fit_params(self):
+        """Get fit parameters."""
+        if self.fit_result:
+            return self.fit_result.values
+        else:
+            raise RuntimeError("Fit has not been performed yet.")
+
+    @property
+    def fit_errors(self):
+        """Get fit parameter errors."""
+        if self.fit_result:
+            return self.fit_result.errors
+        else:
+            raise RuntimeError("Fit has not been performed yet.")
+
+    @property
+    def log_likelihood_value(self):
+        """Get log-likelihood value of the fit."""
+        if self.fit_result:
+            return -self.fit_result.fval  # Negative because we minimize -log(L)
+        else:
+            raise RuntimeError("Fit has not been performed yet.")
+
+class UnbinnedCompositeModel(UnbinnedFitBase):
+    """Composite model class for combining multiple unbinned fit components."""
+
+    def __init__(self, data, param_limits=None):
+        super().__init__(data, param_limits)
+        self.components = []
+
+    def add_component(self, component, weight=1.0):
+        """Add a component to the composite model."""
+        self.components.append((component, weight))
+
+    def pdf(self, x_vals, *params):
+        """Composite probability density function."""
+        result = np.zeros_like(x_vals)
+        param_index = 0
+        for component, weight in self.components:
+            n_params = component.n_params
+            result += weight * component.pdf(x_vals, *params[param_index:param_index+n_params])
+            param_index += n_params
+        return result
+
+    def log_likelihood(self, *params):
+        """Calculate log-likelihood value."""
+        return -np.sum(np.log(self.pdf(self.data, *params)))
+
 # Pre-defined components
 class GaussianComponent(Component):
     def __init__(self):
-        super().__init__(FitModels(1, 0, 1).gaussian, 2)  # mean, std_dev
+        super().__init__(FitModels().gaussian, FitModels().gaussian, 2)  # mean, std_dev
 
 class ExponentialComponent(Component):
     def __init__(self):
-        super().__init__(FitModels(1, 0, 1).exponential, 2)  # amplitude, decay
+        super().__init__(FitModels().exponential, FitModels().exponential, 2)  # amplitude, decay
 
 class LinearComponent(Component):
     def __init__(self):
-        super().__init__(FitModels(1, 0, 1).linear, 2)  # slope, intercept
+        super().__init__(FitModels().linear, FitModels().linear, 2)  # slope, intercept
 
 class ParabolaComponent(Component):
     def __init__(self):
-        super().__init__(FitModels(1, 0, 1).parabola, 3)  # a, b, c
+        super().__init__(FitModels().parabola, FitModels().parabola, 3)  # a, b, c
 
 class BreitWignerComponent(Component):
     def __init__(self):
-        super().__init__(FitModels(1, 0, 1).breit_wigner, 2)  # mass, width
+        super().__init__(FitModels().breit_wigner, FitModels().breit_wigner, 2)  # mass, width
 
 class CrystalBallComponent(Component):
     def __init__(self):
-        super().__init__(FitModels(1, 0, 1).crystal_ball, 4)  # alpha, n, mean, std_dev
+        super().__init__(FitModels().crystal_ball, FitModels().crystal_ball, 4)  # alpha, n, mean, std_dev
 
 class ArgusComponent(Component):
     def __init__(self):
-        super().__init__(FitModels(1, 0, 1).argus_bg, 3)  # m0, c, p
+        super().__init__(FitModels().argus_bg, FitModels().argus_bg, 3)  # m0, c, p
 
-# Existing fit classes (now implemented using CompositeModel)
+# Binned fit models
 class GaussianExpFit(CompositeModel):
     def __init__(self, bins, counts, param_limits=None):
         super().__init__(bins, counts, param_limits)
@@ -275,5 +388,63 @@ class BreitWignerLinearFit(CompositeModel):
 class CrystalBallExpFit(CompositeModel):
     def __init__(self, bins, counts, param_limits=None):
         super().__init__(bins, counts, param_limits)
+        self.add_component(CrystalBallComponent())
+        self.add_component(ExponentialComponent())
+
+# Unbinned fit models
+class UnbinnedGaussianExpFit(UnbinnedCompositeModel):
+    def __init__(self, data, param_limits=None):
+        super().__init__(data, param_limits)
+        self.add_component(GaussianComponent())
+        self.add_component(ExponentialComponent())
+
+class UnbinnedDoubleGaussianExpFit(UnbinnedCompositeModel):
+    def __init__(self, data, param_limits=None):
+        super().__init__(data, param_limits)
+        self.add_component(GaussianComponent())
+        self.add_component(GaussianComponent())
+        self.add_component(ExponentialComponent())
+
+class UnbinnedDoubleGaussianParabolaFit(UnbinnedCompositeModel):
+    def __init__(self, data, param_limits=None):
+        super().__init__(data, param_limits)
+        self.add_component(GaussianComponent())
+        self.add_component(GaussianComponent())
+        self.add_component(ParabolaComponent())
+
+class UnbinnedDoubleGaussianLinearFit(UnbinnedCompositeModel):
+    def __init__(self, data, param_limits=None):
+        super().__init__(data, param_limits)
+        self.add_component(GaussianComponent())
+        self.add_component(GaussianComponent())
+        self.add_component(LinearComponent())
+
+class UnbinnedGaussianArgusFit(UnbinnedCompositeModel):
+    def __init__(self, data, param_limits=None):
+        super().__init__(data, param_limits)
+        self.add_component(GaussianComponent())
+        self.add_component(ArgusComponent())
+
+class UnbinnedGaussianLinearFit(UnbinnedCompositeModel):
+    def __init__(self, data, param_limits=None):
+        super().__init__(data, param_limits)
+        self.add_component(GaussianComponent())
+        self.add_component(LinearComponent())
+
+class UnbinnedBreitWignerExpFit(UnbinnedCompositeModel):
+    def __init__(self, data, param_limits=None):
+        super().__init__(data, param_limits)
+        self.add_component(BreitWignerComponent())
+        self.add_component(ExponentialComponent())
+
+class UnbinnedBreitWignerLinearFit(UnbinnedCompositeModel):
+    def __init__(self, data, param_limits=None):
+        super().__init__(data, param_limits)
+        self.add_component(BreitWignerComponent())
+        self.add_component(LinearComponent())
+
+class UnbinnedCrystalBallExpFit(UnbinnedCompositeModel):
+    def __init__(self, data, param_limits=None):
+        super().__init__(data, param_limits)
         self.add_component(CrystalBallComponent())
         self.add_component(ExponentialComponent())
